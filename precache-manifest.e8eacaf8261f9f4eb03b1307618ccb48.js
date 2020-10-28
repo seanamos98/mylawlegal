887471454de4e0d38bb03cd8a003ad029217797b
Tosin <seanamos98@gmail.com>
2020-10-28 03:39:31 +0100

    Updates

---
+++ precache-manifest.e8eacaf8261f9f4eb03b1307618ccb48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90d7c8e723145ef34353b8d204dcbd56",
+    "url": "/mylawlegal/index.html"
+  },
+  {
+    "revision": "e468c24be95e0cdf910f",
+    "url": "/mylawlegal/static/css/main.88dff0d0.chunk.css"
+  },
+  {
+    "revision": "e727b68466a851eb5847",
+    "url": "/mylawlegal/static/js/2.82d16b16.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/mylawlegal/static/js/2.82d16b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e468c24be95e0cdf910f",
+    "url": "/mylawlegal/static/js/main.b32969be.chunk.js"
+  },
+  {
+    "revision": "9ef193035e086d1c17cc",
+    "url": "/mylawlegal/static/js/runtime-main.befc9dbc.js"
+  }
+]);
